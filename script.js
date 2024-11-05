e6fe958f7908e01de863f20532c6597958168ff8
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-05 11:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(myName,myAge){
+		this.name = myName;
+		this.age = myAge;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(newAge){
+		this.age = newAge;
+	}
+	get age(){
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
